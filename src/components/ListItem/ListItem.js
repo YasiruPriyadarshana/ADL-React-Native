b3c12636994ed02fac5f7a0c03d24ccf710a489f
YasiruPriyadarshana <hly.priyadarshana@gmail.com>
2021-03-04 09:40:04 +0530

    image

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
 const ListItem = (props) => (
     <TouchableOpacity  onPress={props.onItemPressed}>
-        <View style={styles.ListItem}>
+        <View style={styles.listItem}>
+            <Image source={props.placeImage} style={styles.placeImage}/>
             <Text>{props.placeName}</Text>
         </View>
     </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
-    ListItem: {
+    listItem: {
         width: "100%",
         marginBottom: 5,
         padding: 10,
-        backgroundColor: "#eee"
+        backgroundColor: "#eee",
+        flexDirection:"row",
+        alignItems:"center",
+    },
+    placeImage:{
+        marginRight:8,
+        height:40,
+        width:40
     }
 });
 
