d5ba6e9914d5de2b24960925cd99a8d2f725c126
DESKTOP-NRMS3MT\Yasiru <hly.priyadarshana@gmail.com>
2021-03-26 14:36:52 +0530

    image picker

--- src/components/PickImage/PickImage.js
+++ src/components/PickImage/PickImage.js
@@ -2,17 +2,50 @@ import React, { Component } from "react";
 
 import { View, Image, Button, StyleSheet } from "react-native"
 import imagePlaceHolder from "../../assets/background.jpg";
+import * as ImagePicker from 'react-native-image-picker';
 
 class PickImage extends Component {
+    state={
+        response:null
+    }
+
+    imagePickerHandler=()=>{
+        // ImagePicker.launchCamera(
+        //     {
+        //         mediaType: 'photo',
+        //         includeBase64: false,
+        //         maxHeight: 200,
+        //         maxWidth: 200
+        //     },
+        //     (response) => {
+        //         this.setState({
+        //             response:response.uri
+        //         });
+        //     },
+        // )
+        ImagePicker.launchImageLibrary(
+            {
+              mediaType: 'photo',
+              includeBase64: false,
+              maxHeight: 200,
+              maxWidth: 200
+            },
+            (response) => {
+                this.setState({
+                    response:response.uri
+                });
+            },
+        )
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.placeholder}>
-                    <Image source={imagePlaceHolder} style={styles.previewImage} />
+                    <Image source={{uri: this.state.response}} style={styles.previewImage} />
                 </View>
                 <View style={styles.button}>
-                    <Button title="Pick Image" onPress={() => alert('pick image')}/>
+                    <Button title="Pick Image" onPress={this.imagePickerHandler} />
                 </View>
             </View>
         );
@@ -20,9 +53,9 @@ class PickImage extends Component {
 }
 
 const styles = StyleSheet.create({
-    container:{
-        width:"100%",
-        alignItems:"center"
+    container: {
+        width: "100%",
+        alignItems: "center"
     },
     placeholder: {
         borderWidth: 1,
@@ -31,13 +64,14 @@ const styles = StyleSheet.create({
         width: "80%",
         height: 150
     },
-    button:{
-        margin:8
+    button: {
+        margin: 8
     },
-    previewImage:{
-        width:"100%",
-        height:"100%"
-    }
+    previewImage: {
+        width: "100%",
+        height: "100%"
+    },
+
 })
 
 
