58bba1e1d0af8421c87779f484338254fe3ee204
DESKTOP-NRMS3MT\Yasiru <hly.priyadarshana@gmail.com>
2021-04-05 12:07:26 +0530

    firebase fetch

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,30 +1,45 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-    return {
-        type:ADD_PLACE,
-        placeName:placeName,
-        location:location,
-        image:image
+    return  dispatch =>{
+        const placeData={
+            name:placeName,
+            location:location
+        }
+        fetch("https://awesome-places-308711-default-rtdb.firebaseio.com/places.json", {
+            method:"POST",
+            body:JSON.stringify(placeData)
+        })
+        .catch(err=> console.log(err))
+        .then(res=>res.json())
+        .then(parsedRes =>{
+            console.log(parsedRes);
+        });
     };
 };
 
 export const deletePlace = (key) => {
     return {
         type:DELETE_PLACE,
-        placeKey:key,
+        placeKey:key
     };
 };
 
-// export const selectPlace = (key) => {
-//     return {
-//         type:SELECT_PLACE,
-//         placeKey:key
-//     };
-// };
+/*
+type:ADD_PLACE,
+        placeName:placeName,
+        location:location,
+        image:image
+export const selectPlace = (key) => {
+    return {
+        type:SELECT_PLACE,
+        placeKey:key
+    };
+};
 
-// export const deselectPlace = () => {
-//     return {
-//         type:DESELECT_PLACE
-//     };
-// };
+export const deselectPlace = () => {
+    return {
+        type:DESELECT_PLACE
+    };
+};
+*/
